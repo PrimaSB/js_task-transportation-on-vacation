07a436e0f48955117aa56ee36ded4be7242a3aab
Anna Pryimak <sapfira03@gmail.com>
2026-02-26 14:48:10 +0200

    fixed hard values

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,17 @@ function calculateRentalCost(days) {
   const dayCost = 40;
   const totalCost = days * dayCost;
 
-  if (days >= 7) {
-    return totalCost - 50;
+  const LONG_TERM_RENTAL_DAYS = 7;
+  const LONG_TERM_DISCOUNT = 50;
+  const SHORT_TERM_RENTAL_DAYS = 3;
+  const SHORT_TERM_DISCOUNT = 20;
+
+  if (days >= LONG_TERM_RENTAL_DAYS) {
+    return totalCost - LONG_TERM_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return totalCost - 20;
+  if (days >= SHORT_TERM_RENTAL_DAYS) {
+    return totalCost - SHORT_TERM_DISCOUNT;
   }
 
   return totalCost;
